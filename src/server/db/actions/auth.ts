269d6d5d484bb5263e6c5bb3df173b4ca14a6ce2
Krish Prasad <krishkp00@gmail.com>
2025-01-31 15:45:34 -0500

    add error handling for MongoDB insert

--- src/server/db/actions/auth.ts
+++ src/server/db/actions/auth.ts
@@ -1,7 +1,13 @@
+import { CustomError } from "@/utils/types/exceptions";
 import client from "../dbClient";
 import { User } from "../models";
 
 export async function createUser(newUser: User) {
   const db = client.db();
-  await db.collection("users").insertOne(newUser);
+  try {
+    await db.collection("users").insertOne(newUser);
+  } catch (error) {
+    console.error("Failed to insert new user:", error);
+    throw new CustomError(500, "Internal Server Error");
+  }
 }
