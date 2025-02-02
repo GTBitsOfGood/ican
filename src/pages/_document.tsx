3df897189a98c579a00a5b8912e6d5510b67fe97
kennykeni <linkenny917@gmail.com>
2025-02-01 19:10:24 -0500

    change font imports

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,27 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Quantico:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <body className="antialiased">
         <Main />
---
+++ src/styles/font.ts
@@ -0,0 +1,14 @@
+import { Quantico } from "next/font/google";
+import { Pixelify_Sans } from "next/font/google";
+
+export const pixelifySans = Pixelify_Sans({
+  subsets: ["latin"], 
+  weight: ["400", "500", "600", "700"],  
+  variable: "--font-pixelify-sans",  
+});
+
+export const quantico = Quantico({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-quantico",
+});
