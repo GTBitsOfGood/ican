28633b1ce055a0791dfa728c60945dd3f5c40422
Krish Prasad <krishkp00@gmail.com>
2025-02-07 14:51:48 -0500

    create UnauthorizedRoute

---
+++ src/components/UnauthorizedRoute.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { authService } from "@/http/authService";
+
+export default function UnauthorizedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const validateToken = async () => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        return;
+      }
+
+      try {
+        await authService.validateToken();
+        router.push("/");
+      } catch {}
+    };
+
+    validateToken();
+  }, [router]);
+
+  return <>{children}</>;
+}
