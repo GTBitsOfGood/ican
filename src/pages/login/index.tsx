51622245221e10705f7c3a7328ccb0dac9e3140c
Krish Prasad <krishkp00@gmail.com>
2025-02-07 15:08:49 -0500

    add Unauthorized/Authorized components to pages

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { emailIsValid, passwordIsValid } from "@/utils/validation";
 import ErrorBox from "@/components/ErrorBox";
+import UnauthorizedRoute from "@/components/UnauthorizedRoute";
 
 export default function Home() {
   const [email, setEmail] = useState("");
@@ -45,98 +46,100 @@ export default function Home() {
   };
 
   return (
-    <div className="flex h-screen font-quantico bg-cover bg-no-repeat bg-[url('/LoginBackground.svg')] py-2">
-      <div
-        className={`self-center flex flex-col items-center justify-center rounded-[64px] mobile:w-[70%] desktop:w-[45%] bg-white ${loggingIn ? "h-auto" : "h-full"} mx-auto overflow-scroll`}
-      >
-        <Image
-          className="mb-4"
-          src="/icanLogo.svg"
-          alt="Logo"
-          width={248}
-          height={167}
-        />
-        <div className="self-center w-[80%] my-4 text-center text-black text-[28px] font-bold leading-[36px] tracking-[-1.44px]">
-          Adopt & Care for a Supportive Pet Pal for Your Medication Journey!
-        </div>
-        {loggingIn ? (
-          <div className="flex justify-between space-between items-center text-white text-[48px] font-bold text-shadow-default text-stroke-2 text-stroke-default mt-6 mb-4">
-            LOGGING IN
-            <Image
-              className="ml-3 spin"
-              src="/loading.svg"
-              alt="loading"
-              width={60}
-              height={60}
-            />
+    <UnauthorizedRoute>
+      <div className="flex h-screen font-quantico bg-cover bg-no-repeat bg-[url('/LoginBackground.svg')] py-2">
+        <div
+          className={`self-center flex flex-col items-center justify-center rounded-[64px] mobile:w-[70%] desktop:w-[45%] bg-white ${loggingIn ? "h-auto" : "h-full"} mx-auto overflow-scroll`}
+        >
+          <Image
+            className="mb-4"
+            src="/icanLogo.svg"
+            alt="Logo"
+            width={248}
+            height={167}
+          />
+          <div className="self-center w-[80%] my-4 text-center text-black text-[28px] font-bold leading-[36px] tracking-[-1.44px]">
+            Adopt & Care for a Supportive Pet Pal for Your Medication Journey!
           </div>
-        ) : (
-          <>
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col items-center justify-center w-[80%] bg-white rounded-lg"
-            >
-              <div className="text-white self-start text-[32px]/[40px] font-bold text-shadow-default text-stroke-2 text-stroke-default mb-4">
-                Log In
-              </div>
-              <input
-                className={`flex h-16 px-4 items-center gap-[5px] ${emailError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] focus:text-textGrey focus:placeholder-textGrey focus:border-borderGrey self-stretch border-2 bg-white`}
-                type="text"
-                placeholder="Email"
-                name="email"
-                value={email}
-                onChange={handleEmailChange}
+          {loggingIn ? (
+            <div className="flex justify-between space-between items-center text-white text-[48px] font-bold text-shadow-default text-stroke-2 text-stroke-default mt-6 mb-4">
+              LOGGING IN
+              <Image
+                className="ml-3 spin"
+                src="/loading.svg"
+                alt="loading"
+                width={60}
+                height={60}
               />
-              <ErrorBox message={emailError} />
-              <input
-                className={`flex h-16 px-4 items-center gap-[5px] ${passwordError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] focus:text-textGrey focus:placeholder-textGrey focus:border-borderGrey self-stretch border-2 bg-white`}
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-              <ErrorBox message={passwordError} />
-              <p className="text-textGrey self-start font-berlin-sans text-[20px] font-normal underline decoration-solid mb-2 [text-decoration-skip-ink:none]">
-                Forgot Password?
-              </p>
-              <button
-                className="w-full bg-loginGreen text-black h-12 text-[24px]/[32px] text-center mb-4"
-                type="submit"
+            </div>
+          ) : (
+            <>
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center justify-center w-[80%] bg-white rounded-lg"
               >
-                Login
-              </button>
-            </form>
-            <div className="flex flex-col gap-y-6 w-[80%]">
-              <div className="flex items-center justify-center w-full">
-                <div className="border border-textGrey w-full" />
-                <div className="text-textGrey px-4">or</div>
-                <div className="border border-textGrey w-full" />
-              </div>
-              <button
-                className="w-full gap-y-2.5 border-2 flex py-3 justify-center items-center border-solid border-[#000] bg-white text-black h-12 text-[24px]/[32px] text-center mb-4"
-                type="button"
-              >
-                <Image
-                  src="/GoogleSocialIcon.svg"
-                  alt="Google Logo"
-                  width={30}
-                  height={30}
-                />
-                <div className="px-3 text-[24px]/[32px] text-[#000]">
-                  Login with Google
+                <div className="text-white self-start text-[32px]/[40px] font-bold text-shadow-default text-stroke-2 text-stroke-default mb-4">
+                  Log In
                 </div>
-              </button>
-            </div>
-            <div className="text-textGrey text-[20px]">
-              Don&apos;t have an account?{" "}
-              <Link className="underline" href="/register">
-                Sign up
-              </Link>
-            </div>
-          </>
-        )}
+                <input
+                  className={`flex h-16 px-4 items-center gap-[5px] ${emailError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] focus:text-textGrey focus:placeholder-textGrey focus:border-borderGrey self-stretch border-2 bg-white`}
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                <ErrorBox message={emailError} />
+                <input
+                  className={`flex h-16 px-4 items-center gap-[5px] ${passwordError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] focus:text-textGrey focus:placeholder-textGrey focus:border-borderGrey self-stretch border-2 bg-white`}
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                <ErrorBox message={passwordError} />
+                <p className="text-textGrey self-start font-berlin-sans text-[20px] font-normal underline decoration-solid mb-2 [text-decoration-skip-ink:none]">
+                  Forgot Password?
+                </p>
+                <button
+                  className="w-full bg-loginGreen text-black h-12 text-[24px]/[32px] text-center mb-4"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </form>
+              <div className="flex flex-col gap-y-6 w-[80%]">
+                <div className="flex items-center justify-center w-full">
+                  <div className="border border-textGrey w-full" />
+                  <div className="text-textGrey px-4">or</div>
+                  <div className="border border-textGrey w-full" />
+                </div>
+                <button
+                  className="w-full gap-y-2.5 border-2 flex py-3 justify-center items-center border-solid border-[#000] bg-white text-black h-12 text-[24px]/[32px] text-center mb-4"
+                  type="button"
+                >
+                  <Image
+                    src="/GoogleSocialIcon.svg"
+                    alt="Google Logo"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="px-3 text-[24px]/[32px] text-[#000]">
+                    Login with Google
+                  </div>
+                </button>
+              </div>
+              <div className="text-textGrey text-[20px]">
+                Don&apos;t have an account?{" "}
+                <Link className="underline" href="/register">
+                  Sign up
+                </Link>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </UnauthorizedRoute>
   );
 }
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -7,6 +7,7 @@ import {
   passwordIsValid,
 } from "../../utils/validation";
 import ErrorBox from "@/components/ErrorBox";
+import UnauthorizedRoute from "@/components/UnauthorizedRoute";
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -85,114 +86,116 @@ export default function Home() {
   };
 
   return (
-    <div className="flex h-screen bg-cover bg-no-repeat bg-[url('/LoginBackground.svg')] p-2">
-      <div
-        className={`self-center flex flex-col font-quantico items-center justify-center rounded-[64px] mobile:w-[70%] desktop:w-[45%] bg-white ${registering ? "h-auto" : "h-full"} mx-auto overflow-scroll`}
-      >
-        <Image
-          className="mb-4"
-          src="/icanLogo.svg"
-          alt="Logo"
-          width={248}
-          height={167}
-        />
+    <UnauthorizedRoute>
+      <div className="flex h-screen bg-cover bg-no-repeat bg-[url('/LoginBackground.svg')] p-2">
+        <div
+          className={`self-center flex flex-col font-quantico items-center justify-center rounded-[64px] mobile:w-[70%] desktop:w-[45%] bg-white ${registering ? "h-auto" : "h-full"} mx-auto overflow-scroll`}
+        >
+          <Image
+            className="mb-4"
+            src="/icanLogo.svg"
+            alt="Logo"
+            width={248}
+            height={167}
+          />
 
-        {registering ? (
-          <div className="self-center w-[80%] my-4 text-center text-black text-[28px] font-bold leading-[36px] tracking-[-1.44px]">
-            Adopt & Care for a Supportive Pet Pal for Your Medication Journey!
-            <div className="flex justify-center items-center text-white self-start text-[36px] font-bold text-shadow-default text-stroke-2 text-stroke-default mt-6 mb-4">
-              <p className="mr-4">CREATING ACCOUNT</p>
-              <Image
-                className="spin"
-                src="/loading.svg"
-                alt="loading"
-                width={50}
-                height={50}
-              />
+          {registering ? (
+            <div className="self-center w-[80%] my-4 text-center text-black text-[28px] font-bold leading-[36px] tracking-[-1.44px]">
+              Adopt & Care for a Supportive Pet Pal for Your Medication Journey!
+              <div className="flex justify-center items-center text-white self-start text-[36px] font-bold text-shadow-default text-stroke-2 text-stroke-default mt-6 mb-4">
+                <p className="mr-4">CREATING ACCOUNT</p>
+                <Image
+                  className="spin"
+                  src="/loading.svg"
+                  alt="loading"
+                  width={50}
+                  height={50}
+                />
+              </div>
             </div>
-          </div>
-        ) : (
-          <>
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col items-center justify-between w-[80%] bg-white rounded-lg"
-            >
-              <div className="text-white self-start text-[32px]/[40px] font-bold text-shadow-default text-stroke-2 text-stroke-default font-quantico mb-2">
-                Sign Up
-              </div>
-              <input
-                className={`flex h-16 px-4 items-center gap-[5px] ${nameError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] self-stretch border-2 bg-white`}
-                type="text"
-                name="name"
-                placeholder="Name"
-                value={name}
-                onChange={handleNameChange}
-              />
-              <ErrorBox message={nameError} />
-              <input
-                className={`flex h-16 px-4 items-center gap-[5px] ${emailError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] self-stretch border-2 bg-white`}
-                type="email"
-                placeholder="Email"
-                name="email"
-                value={email}
-                onChange={handleEmailChange}
-              />
-              <ErrorBox message={emailError} />
-              <input
-                className={`flex h-16 px-4 items-center gap-[5px] ${passwordError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] self-stretch border-2 bg-white`}
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-              <ErrorBox message={passwordError} />
-              <input
-                className={`flex h-16 px-4 items-center gap-[5px] ${confirmPasswordError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] self-stretch border-2 bg-white`}
-                type="password"
-                placeholder="Confirm Password"
-                name="confirmPassword"
-                value={confirmPassword}
-                onChange={handleConfirmPasswordChange}
-              />
-              <ErrorBox message={confirmPasswordError} />
-              <button
-                className="w-full bg-[#2C3694] text-[24px]/[32px] text-white h-12 font-quantico mb-4"
-                type="submit"
+          ) : (
+            <>
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center justify-between w-[80%] bg-white rounded-lg"
               >
-                Sign Up
-              </button>
-              <div className="flex flex-col gap-y-6 w-full">
-                <div className="flex items-center justify-center w-full">
-                  <div className="border border-textGrey w-full" />
-                  <div className="text-textGrey px-4">or</div>
-                  <div className="border border-textGrey w-full" />
+                <div className="text-white self-start text-[32px]/[40px] font-bold text-shadow-default text-stroke-2 text-stroke-default font-quantico mb-2">
+                  Sign Up
                 </div>
+                <input
+                  className={`flex h-16 px-4 items-center gap-[5px] ${nameError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] self-stretch border-2 bg-white`}
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+                <ErrorBox message={nameError} />
+                <input
+                  className={`flex h-16 px-4 items-center gap-[5px] ${emailError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] self-stretch border-2 bg-white`}
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                <ErrorBox message={emailError} />
+                <input
+                  className={`flex h-16 px-4 items-center gap-[5px] ${passwordError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] self-stretch border-2 bg-white`}
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                <ErrorBox message={passwordError} />
+                <input
+                  className={`flex h-16 px-4 items-center gap-[5px] ${confirmPasswordError === "" ? "text-textGrey placeholder-textGrey border-borderGrey mb-2" : "text-errorRed placeholder-errorRed border-errorRed"} text-[24px]/[32px] self-stretch border-2 bg-white`}
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  value={confirmPassword}
+                  onChange={handleConfirmPasswordChange}
+                />
+                <ErrorBox message={confirmPasswordError} />
                 <button
-                  className="w-full gap-y-2.5 border-2 flex py-3 justify-center items-center border-solid border-[#000] bg-white text-black h-12 text-[24px]/[32px] text-center mb-2"
-                  type="button"
+                  className="w-full bg-[#2C3694] text-[24px]/[32px] text-white h-12 font-quantico mb-4"
+                  type="submit"
                 >
-                  <Image
-                    src="/GoogleSocialIcon.svg"
-                    alt="Google Logo"
-                    width={30}
-                    height={30}
-                  />
-                  <div className="px-3 text-[24px]/[32px] text-[#000]">
-                    Sign Up with Google
-                  </div>
+                  Sign Up
                 </button>
-              </div>
-              <div className="text-textGrey font-quantico text-[20px]">
-                Have an account?{" "}
-                <Link className="underline font-quantico" href="/login">
-                  Login
-                </Link>
-              </div>
-            </form>
-          </>
-        )}
+                <div className="flex flex-col gap-y-6 w-full">
+                  <div className="flex items-center justify-center w-full">
+                    <div className="border border-textGrey w-full" />
+                    <div className="text-textGrey px-4">or</div>
+                    <div className="border border-textGrey w-full" />
+                  </div>
+                  <button
+                    className="w-full gap-y-2.5 border-2 flex py-3 justify-center items-center border-solid border-[#000] bg-white text-black h-12 text-[24px]/[32px] text-center mb-2"
+                    type="button"
+                  >
+                    <Image
+                      src="/GoogleSocialIcon.svg"
+                      alt="Google Logo"
+                      width={30}
+                      height={30}
+                    />
+                    <div className="px-3 text-[24px]/[32px] text-[#000]">
+                      Sign Up with Google
+                    </div>
+                  </button>
+                </div>
+                <div className="text-textGrey font-quantico text-[20px]">
+                  Have an account?{" "}
+                  <Link className="underline font-quantico" href="/login">
+                    Login
+                  </Link>
+                </div>
+              </form>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </UnauthorizedRoute>
   );
 }
