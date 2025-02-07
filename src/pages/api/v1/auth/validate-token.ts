f9a0469734286cc2027b15d3d960fd31ab2d62b6
Krish Prasad <krishkp00@gmail.com>
2025-02-07 14:27:47 -0500

    create validateJsonWebToken endpoint

---
+++ src/pages/api/v1/auth/validate-token.ts
@@ -0,0 +1,22 @@
+import { validateJsonWebToken } from "@/services/auth";
+import { ApiError } from "@/types/exceptions";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { authorization } = req.headers;
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  try {
+    const response = validateJsonWebToken(authorization as string);
+    res.status(200).json(response);
+  } catch (error) {
+    if (error instanceof ApiError) {
+      res.status(error.statusCode).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: (error as Error).message });
+    }
+  }
+}
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,6 +2,7 @@ import {
   AlreadyExistsError,
   BadRequestError,
   DoesNotExistError,
+  UnauthorizedError,
 } from "@/types/exceptions";
 import {
   passwordsAreEqual,
@@ -107,4 +108,20 @@ export async function validateLogin(email: string, password: string) {
   );
 
   return { token };
+}
+
+export async function validateJsonWebToken(authorization: string) {
+  // Get token from the Authorization header
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    throw new UnauthorizedError("No token provided");
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    jwt.verify(token, JWT_SECRET);
+    return { isValid: true };
+  } catch {
+    throw new UnauthorizedError("invalid token");
+  }
 }
