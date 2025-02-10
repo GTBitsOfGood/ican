8fd89794e639fcf5aefb64efca907073e863147c
Nathan Papa <85318153+ShivaniVora@users.noreply.github.com>
2025-02-09 21:12:30 -0500

    Settings modals

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,10 +1,10 @@
 import { Quantico } from "next/font/google";
 import { Pixelify_Sans } from "next/font/google";
 
-export const pixelifySans = Pixelify_Sans({
-  subsets: ["latin"], 
-  weight: ["400", "500", "600", "700"],  
-  variable: "--font-pixelify-sans",  
+export const pixelify = Pixelify_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-pixelify",
 });
 
 export const quantico = Quantico({
