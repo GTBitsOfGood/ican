16f1b1406c56f7f5511bfb5cd8fcc9d01f331090
kennykeni <linkenny917@gmail.com>
2025-02-22 18:53:17 -0500

    change http function name

--- src/app/api/v1/[[...slug]]/route.ts
+++ src/app/api/v1/[[...slug]]/route.ts
@@ -5,7 +5,7 @@ import { NextRequest } from "next/server";
 // This isn't needed at all, but just replaces the 404 page with a JSON response containing "Route not found: nextUrl"
 // Can definitely just be deleted if we want NextJS to just show a 404 page instead
 
-export async function ALL(req: NextRequest) {
+export async function GET(req: NextRequest) {
   try {
     throw new NotFoundError(`Route not found: ${req.nextUrl}`);
   } catch (error) {
@@ -13,4 +13,4 @@ export async function ALL(req: NextRequest) {
   }
 }
 
-export { ALL as GET, ALL as POST, ALL as PUT, ALL as DELETE, ALL as PATCH };
+export { GET as POST, GET as PUT, GET as DELETE, GET as PATCH };
