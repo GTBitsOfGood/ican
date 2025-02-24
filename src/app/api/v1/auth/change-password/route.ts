861d5c0ab5f5623f03739d158687749e5dcbe06c
kennykeni <linkenny917@gmail.com>
2025-02-24 15:25:47 -0500

    change change-password flow

--- src/app/api/v1/auth/change-password/route.ts
+++ src/app/api/v1/auth/change-password/route.ts
@@ -1,24 +1,25 @@
 import { changePassword } from "@/services/forgotPasswordCodes";
-import { UnauthorizedError } from "@/types/exceptions";
 import { handleError } from "@/utils/errorHandler";
 import { validateRoutes } from "@/utils/validateRoute";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(req: NextRequest) {
   try {
-    await validateRoutes(req, req.method, req.nextUrl.pathname.toString());
-
+    const userId = await validateRoutes(
+      req,
+      req.method,
+      req.nextUrl.pathname.toString(),
+    );
     const { password, confirmPassword } = await req.json();
 
-    const authHeader = req.headers.get("authorization");
-    if (!authHeader) {
-      throw new UnauthorizedError("Forbidden");
+    if (!userId) {
+      // Not sure what error to throw here, it would only realistically happen when route maps authorization doesn't match up
+      throw new Error(
+        "Token was not checked when it was expected, ensure route map defines authorization.",
+      );
     }
-    const token = authHeader.split(" ")[1];
-
-    await changePassword(token, password, confirmPassword);
-
-    return NextResponse.json(null, { status: 200 });
+    await changePassword(userId, password, confirmPassword);
+    return new NextResponse(null, { status: 204 });
   } catch (error) {
     return handleError(error);
   }
