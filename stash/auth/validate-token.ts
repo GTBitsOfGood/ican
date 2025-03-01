3f3b99756705a2617e45b608b8cb1a08a3967d80
Krish Prasad <krishkp00@gmail.com>
2025-03-01 12:57:56 -0500

    fix build error

--- stash/auth/validate-token.ts
+++ stash/auth/validate-token.ts
@@ -17,9 +17,9 @@ export default async function handler(
     if (!authorization || !authorization.startsWith("Bearer ")) {
       throw new UnauthorizedError("No token provided");
     }
-    const token = authorization.split(" ")[1];
+    //const token = authorization.split(" ")[1];
 
-    const decodedToken = await validateToken(token);
+    const decodedToken = await validateToken();
     res.status(200).json({ isValid: true, decodedToken: decodedToken });
   } catch (error) {
     if (error instanceof ApiError) {
