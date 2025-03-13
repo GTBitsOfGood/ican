37091768c4c44a96766efed9f32ea041244b698f
Syaam Khandaker <syaamkhandaker@gmail.com>
2025-03-13 11:31:04 -0400

    added cookie gneeration for registration

--- src/app/api/v1/auth/register/route.ts
+++ src/app/api/v1/auth/register/route.ts
@@ -10,14 +10,25 @@ export async function POST(req: NextRequest) {
     await validateRoutes(req, req.method, req.nextUrl.pathname.toString());
     const { name, email, password, confirmPassword } = await req.json();
 
-    const response = await AuthService.register(
+    const authToken = await AuthService.register(
       name,
       email,
       password,
       confirmPassword,
     );
 
-    return NextResponse.json({ token: response }, { status: 201 });
+    const nextResponse = NextResponse.json({}, { status: 204 });
+
+    // set expiration date 3 hours after
+    const expirationDate = new Date();
+    expirationDate.setHours(expirationDate.getHours() + 3);
+
+    nextResponse.cookies.set("auth_token", authToken, {
+      httpOnly: true,
+      expires: expirationDate,
+    });
+
+    return nextResponse;
   } catch (error) {
     return handleError(error);
   }
