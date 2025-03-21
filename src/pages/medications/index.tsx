d7c5b0d64fe8744034f50d9e781a0dd235b1bff4
Shivani Vora <shivanivora797@icloud.com>
2025-03-21 16:59:08 -0500

    Fix build error

--- src/pages/medications/index.tsx
+++ src/pages/medications/index.tsx
@@ -37,7 +37,6 @@ export default function MedicationsPage() {
           clickedIndex !== null && (
             <DeleteMedicationModal
               medication={medications[clickedIndex]}
-              medicationIndex={clickedIndex}
               setDeleteModalVisible={setDeleteModalVisible}
               handleDelete={() => handleMedicationDelete(clickedIndex)}
             />
