cfab4a6a5e876892119e033f86178266062ad6a9
Syaam Khandaker <syaamkhandaker@gmail.com>
2025-04-06 10:25:25 -0400

    updated height for missed dose

--- src/components/modals/MissedDoseModal.tsx
+++ src/components/modals/MissedDoseModal.tsx
@@ -39,7 +39,7 @@ export default function MissedDoseModal({
           "mobile:text-2xl tablet:text-3xl largeDesktop:text-4xl tiny:text-xl minimized:text-2xl small:text-3xl justify-center items-center",
         body: "items-center justify-between",
       }}
-      className="mobile:w-[70%] tablet:w-[65%] desktop:w-[60%] largeDesktop:w-[55%] mobile:h-[40%] tablet:h-[35%] tiny:h-[75%] minimized:h-[67.5%] short:h-[50%] font-quantico font-bold z-50 text-white py-8 px-6 overflow-y-auto rounded-none outline-none"
+      className="mobile:w-[70%] tablet:w-[65%] desktop:w-[60%] largeDesktop:w-[55%] h-fit font-quantico font-bold z-50 text-white py-8 px-6 overflow-y-auto rounded-none outline-none"
       isOpen={isOpen}
       onClose={onClose}
       radius="lg"
