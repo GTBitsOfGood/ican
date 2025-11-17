694ce9e7c1cf6f8acaf08325d4f60cac46106529
Nathan <nathandong68@gmail.com>
2025-11-16 22:37:47 -0500

    icon text visibility

--- src/components/ui/NavButton.tsx
+++ src/components/ui/NavButton.tsx
@@ -48,7 +48,7 @@ const Button: React.FC<ButtonProps> = ({
           </div>
           {drawButton && (
             <div className="mobile:hidden tablet:inline-flex h-fit z-10 justify-center items-center mt-1 ml-3">
-              <span className="font-quantico text-center text-white mobile:text-3xl largeDesktop:text-4xl 4xl:text-5xl font-bold leading-9 text-stroke-4 text-stroke-[#353859] text-shadow-[#4C539B] paint-stroke letter-spacing-ui">
+              <span className="font-quantico text-center text-white mobile:text-3xl largeDesktop:text-4xl 4xl:text-5xl font-bold leading-9 text-stroke-4 text-stroke-[#2B2F58] text-shadow-[#2B2F58] paint-stroke letter-spacing-ui">
                 {buttonType.toUpperCase()}
               </span>
             </div>
