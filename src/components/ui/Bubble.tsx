dacadbdd20a4a9935deb3643981e423f08f75c49
Nathan <nathandong68@gmail.com>
2025-11-17 04:10:52 -0500

    updated responsive speech bubble

--- src/components/ui/Bubble.tsx
+++ src/components/ui/Bubble.tsx
@@ -8,17 +8,32 @@ const Bubble: React.FC<BubbleProps> = ({
   text = "Hey there! Don’t forget to take your medicine at 12:00!!",
 }) => {
   return (
-    <div className="tall:w-72 mobile:w-48 tiny:w-98 tablet:w-60 short:w-84 desktop:w-72 lg:w-80 xl:w-96 2xl:w-[32rem] 4xl:w-[43.75rem] mobile:leading-[1rem] largeDesktop:leading-[4rem]">
-      <div className="w-full mobile:p-4 desktop:p-8 bg-white tiny:text-xl short:text-2xl mobile:text-md tablet:text-xl desktop:text-3xl largeDesktop:text-4xl font-bold text-[#343859] text-center shadow-bubble font-quantico">
-        {text}
-      </div>
-      <div className="h-10 w-10 ml-12 relative">
+    <div className="tall:w-[14rem] mobile:w-[14rem] tiny:w-[14rem] tablet:w-[24rem] short:w-[24rem] desktop:w-[34rem] lg:w-[34rem] xl:w-116 2xl:w-[34rem] 4xl:w-[45.75rem] mobile:leading-[1rem] largeDesktop:leading-[4rem]">
+      <Image
+        src={"/misc/ChatBubbleTop.svg"}
+        alt={"chat bubble top"}
+        width={74}
+        height={65}
+        className="object-fill pointer-events-none w-full h-full"
+      />
+      <div className="relative inline-block -mt-1">
+        <div className="w-full mobile:p-4 desktop:p-8 tiny:text-xl short:text-2xl mobile:text-xl tablet:text-xl desktop:text-3xl largeDesktop:text-4xl font-bold text-[#343859] text-center font-quantico">
+          {text}
+        </div>
         <Image
-          src="/misc/chatBubbleCorner.svg"
-          alt="Chat Bubble Decoration"
+          src={"/misc/ChatBubbleMid.svg"}
+          alt={"chat bubble top"}
           fill
+          className="object-fill pointer-events-none absolute top-0 left-[10] w-full h-full z-[-1] ml-[0.3%]"
         />
       </div>
+      <Image
+        src={"/misc/ChatBubbleBottom.svg"}
+        alt={"chat bubble top"}
+        width={74}
+        height={65}
+        className="object-fill pointer-events-none w-full h-full -mt-1"
+      />
     </div>
   );
 };
