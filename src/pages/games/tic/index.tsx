7cb84fad1226ae79ba0058ead3109a3f7da400f9
EuanHam <euanham05@gmail.com>
2026-02-21 13:07:33 -0500

    fix: remove duplicate route

--- src/pages/games/tic/index.tsx
+++
@@ -1,6 +0,0 @@
-import GameWrapper from "@/components/games/GameWrapper";
-import TicTacToe from "@/components/games/TicTacToe";
-
-export default function TicTacToePage() {
-  return <GameWrapper GameComponent={TicTacToe} />;
-}
