6e8efbd311ebd8f69adcd3d3dd4d082fe6e263cd
sailalithkanumuri8 <saikanbolt@gmail.com>
2026-04-02 16:59:25 -0400

    Updated trivia

--- src/pages/games/trivia.tsx
+++ src/pages/games/trivia.tsx
@@ -7,6 +7,7 @@ export default function TriviaGamePage() {
     <GameWrapper
       GameComponent={TriviaGame}
       gameName={GameName.TRIVIA}
+      manualRecording
       gameAreaClassName="right-[12%]"
     />
   );
